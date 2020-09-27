8ac64e1d0012aec51f5952f13861a6f27e4c0ddc
Christian Pari <ChristianPari@outlook.com>
2020-09-27 17:09:32 -0400

    Refactored to use the new BookListContext I created, handles book list state

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 // contexts
 import { useTheme } from '../Contexts/ThemeContext'
+import { useBooks, useBooksUpdate } from '../Contexts/BookListContext'
+
+// components
+import NewBookForm from './NewBookForm'
 
 export default function BookList() {
   const { isLightTheme, light, dark } = useTheme();
@@ -14,14 +18,24 @@ export default function BookList() {
   const liStyle = {
     background: theme.ui
   }
-  
+
   return (
     <div className='book-list' style={divStyle}>
       <ul>
-        <li style={liStyle}>The Lighting Thief</li>
-        <li style={liStyle}>Sea of Monsters</li>
-        <li style={liStyle}>The Titan's Curse</li>
+        { useBooks().map(book => {
+          return (
+            <li
+              key={book.id}
+              style={liStyle}
+            >
+              {book.title}
+            </li>
+          )
+        })}
       </ul>
+      <NewBookForm 
+        addBook={useBooksUpdate()}
+      />
     </div>
   )
 }
