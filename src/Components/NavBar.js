bbc5bd1da29667cc0b37db8f6509beda67201c64
Christian Pari <ChristianPari@outlook.com>
2020-09-27 15:05:00 -0400

    Implemented another Context hook, simple use of auth to just say if logged in or out and changes via onlclick of the div element with the text in it

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,14 +2,23 @@ import React from 'react'
 
 // contexts
 import { useTheme } from '../Contexts/ThemeContext'
+import { useAuth, useAuthUpdate } from '../Contexts/AuthContext'
 
 export default function NavBar() {
-  const { isLightTheme, light, dark } = useTheme();
-  const theme = isLightTheme ? light : dark;
+  const { isLightTheme, light, dark } = useTheme()
+  const theme = isLightTheme ? light : dark
+
+  const { isAuthenticated: auth } = useAuth()
 
   return (
     <nav style={{background: theme.ui, color: theme.text}}>
       <h1>Context App</h1>
+      <div
+        onClick={useAuthUpdate()}
+        style={{cursor: 'pointer'}}
+      >
+        { auth ? 'Logged In' : 'Logged Out' }
+      </div>
       <ul>
         <li>Home</li>
         <li>About</li>
