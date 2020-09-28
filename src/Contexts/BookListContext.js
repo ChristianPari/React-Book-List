8fd0a09575725bc5d7c8cee46e0f2271ff5f8d48
Christian Pari <ChristianPari@outlook.com>
2020-09-28 18:42:49 -0400

    Added another function that handles removing data from the state

--- src/Contexts/BookListContext.js
+++ src/Contexts/BookListContext.js
@@ -14,18 +14,31 @@ export function useBooksUpdate() {
 
 export function BookListContextProvider({ children }) {
   const [books, setBooks] = useState([
-    { title: 'The Lighting Thief', id: 1},
-    { title: 'Sea of Monsters', id: 2},
-    { title: "The Titan's Curse", id: 3}
+    { 
+      title: 'The Lighting Thief', 
+      author: 'Rick Riordan', 
+      id: 1},
+    { 
+      title: 'Sea of Monsters', 
+      author: 'Rick Riordan', 
+      id: 2},
+    { 
+      title: "The Titan's Curse", 
+      author: 'Rick Riordan', 
+      id: 3}
   ])
   
-  const addBook = (title) => {
-    setBooks([...books, { title, id: v1() }])
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: v1() }])
+  }
+
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id))
   }
 
   return (
     <BooksContext.Provider value={books}>
-      <BooksUpdateContext.Provider value={addBook}>
+      <BooksUpdateContext.Provider value={{addBook, removeBook}}>
         { children }
       </BooksUpdateContext.Provider >
     </BooksContext.Provider>
